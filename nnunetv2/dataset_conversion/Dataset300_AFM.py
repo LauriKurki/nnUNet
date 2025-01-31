fd2034fd4e4a818fc58b9bb211e4e5f233bb62d5
Lauri Kurki <lauri.v.kurki@gmail.com>
2025-01-31 12:46:02 +0200

    updates

--- nnunetv2/dataset_conversion/Dataset300_AFM.py
+++ nnunetv2/dataset_conversion/Dataset300_AFM.py
@@ -1,44 +1,18 @@
 import os
-import tqdm
+import shutil
+import tarfile
+import h5py
+import multiprocessing as mp
 import numpy as np
-import tensorflow as tf
+import tqdm.contrib.concurrent
 
 import nibabel as nib
-
 from batchgenerators.utilities.file_and_folder_operations import *
-from nnunetv2.dataset_conversion.generate_dataset_json import generate_dataset_json
 from nnunetv2.paths import nnUNet_raw
-import SimpleITK as sitk
+from nnunetv2.dataset_conversion.generate_dataset_json import generate_dataset_json
 
+from typing import List, Tuple
 
-def get_dataloader(folder: str):
-    files = sorted(os.listdir(folder))
-    files = [
-        os.path.join(folder, f)
-        for f in files
-        if f.startswith("afms_")
-    ]
-
-    # Get the element spec of the first file.
-    element_spec = tf.data.Dataset.load(files[0]).element_spec
-    ds = tf.data.Dataset.from_tensor_slices(files)
-    # Shuffle the files.
-    ds = ds.interleave(
-        lambda path: tf.data.Dataset.load(path, element_spec=element_spec),
-        num_parallel_calls=tf.data.AUTOTUNE,
-        deterministic=True,
-    )
-
-    # Compute the segmentation map.
-    ds = ds.map(
-        compute_segmentation_map,
-        num_parallel_calls=tf.data.AUTOTUNE
-    )
-
-    # Prefetch the data.
-    ds = ds.prefetch(tf.data.AUTOTUNE).as_numpy_iterator()
-
-    return ds
 
 def compute_segmentation_map(sample):
     image = sample["x"]
@@ -46,18 +20,18 @@ def compute_segmentation_map(sample):
     sw = sample["sw"]
 
     # The image has to be transposed.
-    image = tf.transpose(image, [1, 0, 2])
+    image = np.transpose(image, [1, 0, 2])
 
-    z_max = tf.math.reduce_max(xyz[:, 2])
-    x = tf.linspace(sw[0,0], sw[1,0], 128)
-    y = tf.linspace(sw[0,1], sw[1,1], 128)
-    z = tf.linspace(z_max, z_max-1.0, 10)
+    z_max = np.max(xyz[:, 2])
+    x = np.linspace(sw[0,0], sw[1,0], 128)
+    y = np.linspace(sw[0,1], sw[1,1], 128)
+    z = np.linspace(z_max, z_max-1.0, 10)
 
     # Create a meshgrid.
-    X, Y, Z = tf.meshgrid(x, y, z, indexing='xy')
+    X, Y, Z = np.meshgrid(x, y, z, indexing='xy')
 
     # Create segmentation map.
-    seg = tf.zeros_like(X)
+    seg = np.zeros_like(X)
 
     for atom in xyz:
         # Skip padding
@@ -66,7 +40,7 @@ def compute_segmentation_map(sample):
 
         # Compute the distance to the atom.
         m = (X - atom[0])**2 + (Y - atom[1])**2 + (Z - atom[2])**2
-        m = tf.where(m < 0.2, atom[-1], 0.)
+        m = np.where(m < 0.2, atom[-1], 0.)
 
         # Add the atom to the segmentation map.
         seg += m
@@ -78,33 +52,61 @@ def compute_segmentation_map(sample):
         "xyz": xyz
     }
 
+def generate_nnunet_samples(
+    input_file: str,
+    output_dir: str,
+    indices: List[Tuple[str, int]],
+    counter_indices: List[int],
+    start: int,
+    end: int,
+):
+    """Generate a chunk of nnUNet samples from the AFM dataset."""
+    tar_path = os.path.join(output_dir, f"chunk_{start:06d}_{end:06d}.tar")
 
-def convert_tensorflow_afm(tensorflow_dir: str, dataset_id: int):
-    task_name = "AFMRebias10"
+    for i, ((mode, h5_idx), idx) in enumerate(zip(indices[start:end], counter_indices)):
+        with h5py.File(input_file, 'r') as f:
+            sample = {
+                "x": f[mode]['X'][h5_idx],
+                "xyz": f[mode]['xyz'][h5_idx],
+                "sw": f[mode]['sw'][h5_idx],
+            }
 
-    ds = get_dataloader(tensorflow_dir)
+        sample = compute_segmentation_map(sample)
 
-    target_dataset_name = f'Dataset{dataset_id:03}_{task_name}'
-
-    maybe_mkdir_p(join(nnUNet_raw, target_dataset_name))
-    imagesTr = join(nnUNet_raw, target_dataset_name, 'imagesTr')
-    labelsTr = join(nnUNet_raw, target_dataset_name, 'labelsTr')
-    maybe_mkdir_p(imagesTr)
-    maybe_mkdir_p(labelsTr)
-
-    for i, sample in tqdm.tqdm(enumerate(ds)):
-        image = sample["x"]
+        # Convert sample into nifti format
+        x = sample["x"]
         seg = sample["seg"]
 
-        # Cast image to float16 and seg to uint8
-        image = image.astype(np.float32)
+        x = x.astype(np.float32)
         seg = seg.astype(np.uint8)
 
-        image_path = os.path.join(imagesTr, f"afm_{i+1:06d}_0000.nii.gz")
-        seg_path = os.path.join(labelsTr, f"seg_{i+1:06d}.nii.gz")
+        x_path = os.path.join(output_dir, f"imagesTr/afm_{idx:06d}_0000.nii.gz")
+        seg_path = os.path.join(output_dir, f"labelsTr/afm_{idx:06d}_0000.nii.gz")
 
-        nib.save(nib.Nifti1Image(image, affine=None), image_path)
+        # Save the images
+        nib.save(nib.Nifti1Image(x, affine=None), x_path)
         nib.save(nib.Nifti1Image(seg, affine=None), seg_path)
+
+    # Create a tar file
+    with tarfile.open(tar_path, 'w') as tar:
+        tar.add(os.path.join(output_dir, 'imagesTr'), arcname='imagesTr')
+        tar.add(os.path.join(output_dir, 'labelsTr'), arcname='labelsTr')
+
+    # Remove the images and labels but keep the imagesTr and labelsTr directories
+    shutil.rmtree(os.path.join(output_dir, 'imagesTr'))
+    shutil.rmtree(os.path.join(output_dir, 'labelsTr'))
+
+
+def generate_nnunet_samples_wrapper(args):
+    """Wrapper for generate_nnunet_samples."""
+    generate_nnunet_samples(*args)
+
+
+def main(args):
+    input_file = args.input_file
+    num_workers = args.num_workers
+    chunk_size = args.chunk_size
+    dataset_id = args.d
 
     labels = {
         "background": 0,
@@ -113,16 +115,53 @@ def convert_tensorflow_afm(tensorflow_dir: str, dataset_id: int):
         "Na": 11, "Mg": 12, "Al": 13, "Si": 14, "P": 15, "S": 16, "Cl": 17, "Ar": 18,
         "K": 19, "Ca": 20, "Sc": 21, "Ti": 22, "V": 23, "Cr": 24, "Mn": 25, "Fe": 26, "Co": 27, "Ni": 28, "Cu": 29, "Zn": 30,
         "Ga": 31, "Ge": 32, "As": 33, "Se": 34, "Br": 35,
-    },
-    n_samples = len(
-        os.listdir(image_path)
+    }
+
+    # Get dataset sizes
+    modes = ["train", "val", "test"]
+    mode_sizes = {}
+    for mode in modes:
+        with h5py.File(input_file, 'r') as f:
+            mode_sizes[mode] = len(f[mode]['X'])
+    total_size = sum(mode_sizes.values())
+    print(f"Dataset {dataset_id}: {total_size} samples")
+
+    # Create a list of tuples for each sample (mode, h5_idx)
+    indices = [(mode, h5_idx) for mode in modes for h5_idx in range(mode_sizes[mode])]
+
+    # Create dataset directories
+    task_name = "AFMRebias10"
+    target_dataset_name = f'Dataset{dataset_id:03}_{task_name}'
+    maybe_mkdir_p(join(nnUNet_raw, target_dataset_name))
+    imagesTr = join(nnUNet_raw, target_dataset_name, 'imagesTr')
+    labelsTr = join(nnUNet_raw, target_dataset_name, 'labelsTr')
+    maybe_mkdir_p(imagesTr)
+    maybe_mkdir_p(labelsTr)
+
+    # Create a list of arguments to pass to the wrapper function
+    args_list = [
+        (
+            input_file,
+            join(nnUNet_raw, target_dataset_name),
+            indices,
+            list(range(len(indices))),
+            start,
+            start + chunk_size
+        ) for start in range(0, total_size, chunk_size)
+    ]
+
+    tqdm.contrib.concurrent.process_map(
+        generate_nnunet_samples_wrapper,
+        args_list,
+        max_workers=num_workers,
     )
 
+    # Finally, generate the dataset JSON
     generate_dataset_json(
         join(nnUNet_raw, target_dataset_name),
         {0: 'AFM'},
         labels,
-        n_samples,
+        total_size,
         '.nii.gz',
         None,
         target_dataset_name,
@@ -130,17 +169,36 @@ def convert_tensorflow_afm(tensorflow_dir: str, dataset_id: int):
         #reference='https://aortaseg24.grand-challenge.org/',
         #license='see ref'
     )
-
+    
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'input_folder',
+        'input_file',
         type=str,
-        help="The generated AFM dataset in TF format (must have afms_XXXXX subfolders)"
+        help="The generated AFM dataset in H5 format (must have afms_XXXXX subfolders)"
+    )
+    parser.add_argument(
+        '-n', '--num_workers',
+        type=int,
+        default=4,
+        help="Number of workers for parallel processing"
+    )
+    parser.add_argument(
+        '-c', '--chunk_size',
+        type=int,
+        default=100,
+        help="Chunk size for parallel processing"
     )
     parser.add_argument('-d', required=False, type=int, default=300, help='nnUNet Dataset ID, default: 300')
     args = parser.parse_args()
-    tf_base = args.input_folder
-    convert_tensorflow_afm(tf_base, args.d)
+    input_file = args.input_file
+    num_workers = args.num_workers
+    chunk_size = args.chunk_size
+    dataset_id = args.d
+
+    num_workers = 4
+    chunk_size = 1024
+
+    main(args)
